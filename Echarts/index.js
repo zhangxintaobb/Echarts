681554f59a832d8a3b66202c11cc6c751b41da1e
zhangxintaobb <1309526743@qq.com>
2020-03-25 14:02:19 +0800

    new

--- Echarts/index.js
+++ Echarts/index.js
@@ -39,4 +39,23 @@ $(function() {
     };
     //使用刚刚指定的配置项和数据项显示图表
     myChart.setOption(option);
+    var data =[
+        ['java',1,'降','-0.01%'],
+        ['C',2,'升','+2.44%'],
+        ['Python',3,'升','+1.41%'],
+        ['C++',4,'降','-2.58%'],
+        ['C#',5,'升','+2.07%'],
+        ['Visual Basic .NET',6,'降','-1.17%'],
+        ['JavaScript',7,'降','-0.85%']
+    ];
+      var container = document.getElementById('example');
+      new Handsontable(container, {
+        data: data,
+        rowHeights: 50,
+        colWidths: 270,
+        className: "htCenter htMiddle",
+        colHeaders: true,
+        colHeaders: ['语言名称','排名','升或降','变化幅度'],
+
+    });
   });
